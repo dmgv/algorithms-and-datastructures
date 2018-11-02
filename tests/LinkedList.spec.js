2ede3ce9de18d1b7194c8dd8f3e61b341b831f72
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-11-02 11:02:15 -0300

    Likedlist

---
+++ tests/LinkedList.spec.js
@@ -0,0 +1,15 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+var LL = new LinkedList();
+
+console.log(LL);
+console.log("Diego Menzes");
